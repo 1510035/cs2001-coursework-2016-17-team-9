9d2785df7d04f36bf476e54be89a09571faf1380
shaylan <shaylanhirani@hotmail.com>
2017-01-09 14:56:18 +0000

    Delete init.php

--- init.php
+++
@@ -1,7 +0,0 @@
- <?php  
- $db_name = "webappdb";  
- $mysql_user = "root";  
- $mysql_pass = "";  
- $server_name = "localhost";  
- $con = mysqli_connect($server_name,$mysql_user,$mysql_pass,$db_name);  
- ?>  
