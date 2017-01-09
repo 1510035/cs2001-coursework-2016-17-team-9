9481c00be2d0cfec75e950b1f8721bb8c1999d2a
shaylan <shaylanhirani@hotmail.com>
2017-01-09 14:56:30 +0000

    Delete register.php

--- register.php
+++
@@ -1,8 +0,0 @@
- <?php  
- require "init.php";  
-$name = $_POST["user"];  
-$user_name = $_POST["user_name"];  
-$user_pass = $_POST["user_pass"];  
- $sql_query = "insert into user_info values('$name','$user_name','$user_pass');";  
- mysqli_query($con,$sql_query);
- ?>
